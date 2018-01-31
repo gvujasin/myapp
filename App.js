fd878e668cced2c56c447757ead1812fe62bbeee
Goran <gogo@Gorans-MacBook-Pro-4.local>
2018-01-31 21:42:24 +0100

    boilerplate and basics of flatlist setup

--- App.js
+++ App.js
@@ -1,37 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Platform, StyleSheet, Text, View, FlatList } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <FlatList data={[{name:'bob'}, {name: 'tim'}]}
+        keyExtractor={(item, index) => index}
+        renderItem={({item}) => <Text>{item.name}</Text>}/>
       </View>
     );
   }
@@ -39,19 +16,11 @@ export default class App extends Component<{}> {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 15,
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  
 });
